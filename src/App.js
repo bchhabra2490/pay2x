3f179052d2ec5e5a82b90716da2c1d5d40c187e8
bchhabra2490 <bchhabra2490@gmail.com>
2017-11-26 13:19:33 +0530

    Get API products/1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Carousel, FormControl, FormGroup, Button,Table} from 'react-bootstrap';
+import axios from 'axios';
+
 
 class CustomCarousel extends Component{
   render(){
+    console.log(this.props.images);
+    let carousalItem = [];
+    this.props.images.split(',').map(function(image,i){
+      carousalItem.push(<Carousel.Item key={i}>
+        <img width={900} height={500} alt="900x500" src={'/assets/'+image} style = {{width:400,height:300}}  />
+      </Carousel.Item>)
+    });
+
     return(
       <Carousel className="col-xs-12 col-md-4 col-sm-6 col-lg-4 ">
-      <Carousel.Item>
-        <img width={900} height={500} alt="900x500" src="/assets/image1.png" style = {{width:400,height:300}}  />
-        {/* <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width={900} height={500} alt="900x500" src="/assets/image2.png" style = {{width:400,height:300}}/>
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width={900} height={500} alt="900x500" src="/assets/image2.png"style = {{width:400,height:300}}/>
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-    </Carousel>
+        {carousalItem}
+      </Carousel>
     )
   }
 }
@@ -41,29 +31,29 @@ class DetailTable extends Component{
           <tbody>
           <tr>
             <td>Product Id</td>
-            <td>1</td>
+            <td>{this.props.detail.id}</td>
           </tr>
           <tr>
             <td>Product Name</td>
-            <td>Product Name</td>
+            <td>{this.props.detail.name}</td>
           </tr>
           <tr>
             <td>Company Name</td>
-            <td>Company Name</td>
+            <td>{this.props.detail.company_name}</td>
           </tr>
           <tr>
             <td>Price</td>
-            <td>Rs 12000</td>
+            <td>{this.props.detail.price}</td>
           </tr>
           <tr>
             <td>Rating</td>
-            <td>4/5</td>
+            <td>{this.props.detail.rating}/5</td>
           </tr>
           </tbody>
         </Table>
         <div>
           <h4>Product Description</h4>
-          <p style={{textAlign:'Justify'}} className = "col-lg-12 col-md-12">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <p style={{textAlign:'Justify'}} className = "col-lg-12 col-md-12">{this.props.detail.desc}</p>
         </div>
         </div>
       </div>
@@ -90,13 +80,30 @@ class ReviewForm extends Component{
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      products:{},
+      isLoading:true
+    }
+    const url = 'https://internship-pay2x.herokuapp.com';
+    axios.get(url+'/api/products/1').then((response)=>{
+      console.log(response.data.response[0])
+      this.setState({
+        products:response.data.response[0],
+        isLoading:false
+      })
+    }).catch(function(error){
+      console.log(error);
+    })
+  }
   render() {
     return (
       <div className="App">
         <div>
           <div className = "row">
-          <CustomCarousel />
-          <DetailTable />
+          {!this.state.isLoading && <CustomCarousel images = {this.state.products.images}/>}
+          {!this.state.isLoading && <DetailTable detail = {this.state.products} />}
           </div>
           <div style={{marginTop:20}} className="container">
             <hr/>
