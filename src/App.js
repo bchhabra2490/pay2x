066f283a7b6ec26b296eede396e1fb8ba7e594fe
bchhabra2490 <bchhabra2490@gmail.com>
2017-11-26 12:29:35 +0530

    Improved UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Carousel, FormControl, FormGroup, Button} from 'react-bootstrap';
+import {Carousel, FormControl, FormGroup, Button,Table} from 'react-bootstrap';
 
 class CustomCarousel extends Component{
   render(){
     return(
-      <Carousel>
+      <Carousel className="col-xs-12 col-md-4 col-sm-6 col-lg-4 ">
       <Carousel.Item>
         <img width={900} height={500} alt="900x500" src="/assets/image1.png" style = {{width:400,height:300}}  />
         {/* <Carousel.Caption>
@@ -32,50 +32,60 @@ class CustomCarousel extends Component{
   }
 }
 
-class ReviewForm extends Component{
+class DetailTable extends Component{
   render(){
     return(
-      <form>
-        <FormGroup>
-          <FormControl type="text" id="filter" placeholder = "Rating" style={{background:'black',color:'white'}}/>
-        </FormGroup>
-        <FormGroup>
-          <FormControl componentClass="textarea" placeholder="Write your review" style={{background:'black',color:'white'}} />
-        </FormGroup>
-        <Button type="submit" className = "btn btn-primary">
-          Submit
-        </Button>
-      </form> 
+      <div>
+        <div style = {{marginTop:10}} className="col-xs-12 col-md-8 col-sm-6 col-lg-8" >
+        <Table striped bordered hover>
+          <tbody>
+          <tr>
+            <td>Product Id</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>Product Name</td>
+            <td>Product Name</td>
+          </tr>
+          <tr>
+            <td>Company Name</td>
+            <td>Company Name</td>
+          </tr>
+          <tr>
+            <td>Price</td>
+            <td>Rs 12000</td>
+          </tr>
+          <tr>
+            <td>Rating</td>
+            <td>4/5</td>
+          </tr>
+          </tbody>
+        </Table>
+        <div>
+          <h4>Product Description</h4>
+          <p style={{textAlign:'Justify'}} className = "col-lg-12 col-md-12">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        </div>
+      </div>
     )
   }
 }
 
-class Details extends Component{
+class ReviewForm extends Component{
   render(){
     return(
-      <div>
-        <div>
-          <h2>Hello World</h2>
-          <h3>Company Name</h3>
-          <br/>
-          <h6>Price</h6>
-          <h4>Rs 12000</h4>
-          <br/>
-          <h5>Product Quality:Excellent</h5>
-          <br/>
-          <h4>Product Id</h4>
-          <h4>1</h4>
-          <br/>
-          <h4>Product Ratings</h4>
-          <h5>4.4/5</h5>
-          <h4><u>Detailed Description</u></h4>
-          <h5>Good Product</h5>
-          <h4><u>Reviews</u></h4>
-          <h5>Good Product</h5>
-        </div>
-        <ReviewForm />
-      </div>
-    );
+        <form className="col-xs-12 col-md-6 col-sm-6 col-lg-6 ">
+          <FormGroup>
+            <FormControl type="text" id="filter" placeholder = "Rating" style={{background:'black',color:'white'}}/>
+          </FormGroup>
+          <FormGroup>
+            <FormControl componentClass="textarea" placeholder="Write your review" style={{background:'black',color:'white'}} rows="10"/>
+          </FormGroup>
+          <Button type="submit" className = "btn btn-primary">
+            Submit
+          </Button>
+        </form> 
+    )
   }
 }
 
@@ -83,12 +93,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        <div className="col-xs-12 col-md-4 col-sm-6 col-lg-4 col-sm-offset-3 col-md-offset-4 col-lg-offset-4">
+        <div>
+          <div className = "row">
           <CustomCarousel />
-          <Details />
+          <DetailTable />
+          </div>
+          <div style={{marginTop:20}} className="container">
+            <hr/>
+            <h4 style={{textAlign:'Left'}}>Customer Reviews</h4>
+            <div style={{overflowY:'scroll',maxHeight:200}}>
+              <ul style={{textAlign:'Left',listStyleType:'None'}}>
+                <li>
+                  <h4>Rating</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc</p>
+                </li>
+              </ul>
+            </div>
+            <hr/>
+            <ReviewForm />
+          </div>
         </div>
-        
       </div>
     );
   }
